17238c0cab9f711019f02b57536c3aa163ed1ca4
annapareddypraveenkumar <annapareddypraveenkumar23@gmail.com>
2026-07-31 12:25:58 +0530

    site map

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://youworship.world",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
+}
